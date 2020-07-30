7dba5a3c724da26b97263438d3c5695543c7a9e3
Farhanul Karim <farhanulkarim3770@gmail.com>
2020-07-30 19:28:10 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -7,28 +7,27 @@ end
 
 
 def subtraction(num1, num2)
-  puts sub = num1 - num2
+  num1 - num2
 end
-subtraction(10,5) 
+
 
 def division(num1, num2)
-  puts divide = (num1/num2) 
+  (num1/num2) 
 end
-division(50,2)
 
 
 def multiplication(num1, num2)
-  puts multiply = (num1 * num2)  
+  (num1 * num2)  
 end
-multiplication(4,30) 
+
 
 def modulo(num1, num2)
-  puts mod = num1/num2
+  num1%num2
 end
-modulo(34,5)
+
 
 def square_root(num)
-  puts Math.sqrt(num)
+  Math.sqrt(num)
 end
-square_root(81)
+
 
